343440272249c93e7162871f59e87a9b2c46113a
justinli00 <71279849+justinli00@users.noreply.github.com>
2022-06-15 17:33:08 -0700

    Up and running
    It's just a thing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import * as serviceWorker from './serviceWorker'
+import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App';
 import Header from'./components/Header'
 import Footer from'./components/Footer'
 
 const routing = (
-  <Router>
+  <BrowserRouter>
     <React.StrictMode>
       <Header />
-      <Switch>
-        <Route exact path ="/"component={App}/>
-      </Switch>
+      <Routes>
+        <Route 
+          path ="/"
+          element={<App/>}
+        />
+      </Routes>
       <Footer/>
     </React.StrictMode>
-  </Router>
+  </BrowserRouter>
 )
+
+const root = createRoot(document.getElementById('root'));
+root.render(routing);
+
+reportWebVitals();
